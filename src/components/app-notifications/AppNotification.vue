f5d670cd6d189bd8459f89fd6a2d95e1647dee95
Justin Hays <jhays@TRIDIGITAL.COM>
2019-07-10 13:25:21 -0500

    pop up notification complete

--- src/components/app-notifications/AppNotification.vue
+++ src/components/app-notifications/AppNotification.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="app-notification" :class="{ 'is-opened': isOpened }" v-on:click.self="closeNotification">
-        <transition name="swipe">
+        <component class="notification" :is="notification"></component>
+        <!-- <transition name="swipe">
             <component class="notification" :is="notification"></component>
-        </transition>
+        </transition> -->
     </div>
 </template>
 
@@ -59,17 +60,17 @@ export default {
 /*UPDATE THE ANIMATION AND STYLES FOR THE NOTIFICATIONS ___ STILL USINGN SIDE WIDGET*/
 .app-notification .header {
     position: relative;
-    height: 60px;
+    height: 35px;
     border-bottom: 2px solid #777;
-    background-color: #444;
+    background-color: #dbc539;
 }
 
 .app-notification .header .close-btn {
     position: absolute;
     padding: 5px;
     right: 10px;
-    top: 5px;
-    font-size: 40px;
+    top: 3px;
+    font-size: 30px;
     color: #fff;
 }
 
@@ -77,46 +78,40 @@ export default {
     cursor: pointer;
 }
 
+ .app-notification .content {
+    padding: 15px 20px;
+ }   
+
 </style>
 <style scoped>
 .app-notification {
     position: fixed;
-    left: 100%;
-    opacity: 0;
+    left: calc(50% - 200px);
     display: flex;
     justify-content: flex-end;
-    width: 100%;
-    height: 100vh;
-    top: 0;
+    width: 500px;
+    top: -500px;
     z-index: 10;
-    background-color: rgba(0,0,0,0.7);
+    transform: scale(0);
+    transform-origin: top center;
 
-    transition: left 1ms ease-in, opacity 150ms ease-out;
+    box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.5);
+
+    transition: top 250ms ease-out, transform 250ms ease-out; 
 }
 
 .app-notification.is-opened {
-    left: 0;
-    opacity: 1;
+    top: 20px;
+    transform: scale(1);
 }
 
 .app-notification .notification {
     position: relative;
-    left: 0;
-    width: 400px;
+    width: 100%;
     background-color: #fff;
 }
 
-.app-notification .swipe-enter-active, .app-notification .swipe-leave-active {
-    transition: left 300ms ease-out 50ms; 
-}
-
-.app-notification .swipe-enter, .app-notification .swipe-leave-to {
-    left: 100%;
-}
-
 @media screen and (max-width: 500px) {
-    .app-notification .widget {
-        width: 100%;
-    }
+
 }
 </style>
